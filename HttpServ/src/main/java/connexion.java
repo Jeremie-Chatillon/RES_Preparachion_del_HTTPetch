fe148f140470fa45251ebffdcdee70d5f2aac8b8
zutt <yannlederrey@gmail.com>
2018-05-25 09:26:28 +0200

    essaie d'utilisation de http

---
+++ HttpServ/src/main/java/connexion.java
@@ -0,0 +1,14 @@
+
+public class connexion implements Runnable {
+
+    public void run() {
+
+/*
+        Request.Get("http://somehost/")
+                .connectTimeout(1000)
+                .socketTimeout(1000)
+                .execute().returnContent().asString();
+
+*/
+    }
+}
---
+++ HttpServ/src/main/java/starter.java
@@ -0,0 +1,7 @@
+public class starter {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+        connexion con = new connexion();
+        con.run();
+    }
+}
