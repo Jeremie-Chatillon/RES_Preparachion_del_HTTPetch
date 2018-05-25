818768935ac1fa3faa167c5581bdcdeeac33bf60
zutt <yannlederrey@gmail.com>
2018-05-25 10:05:44 +0200

    get fonctionnel

--- HttpServ/src/main/java/connexion.java
+++ HttpServ/src/main/java/connexion.java
@@ -1,14 +1,12 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
-public class connexion implements Runnable {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    public void run() {
 
-/*
-        Request.Get("http://somehost/")
-                .connectTimeout(1000)
-                .socketTimeout(1000)
-                .execute().returnContent().asString();
-
-*/
-    }
-}
